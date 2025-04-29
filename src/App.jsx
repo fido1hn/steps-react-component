be6d612c4297d7ea2689ffd28baac4fab4c5536b
fido1hn <confidoekeanya@gmail.com>
2025-04-29 04:49:37 +0100

    refactor message to reusable component and children prop

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
   return (
     <div>
       <Steps />
-      {/* <Steps /> */}
     </div>
   );
 }
@@ -39,9 +38,8 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
             <Button onClick={handlePrevious} textColor="#fff" bgColor="#7950f2">
               <span>👈</span> Previous
@@ -52,6 +50,14 @@ function Steps() {
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}:</h3> {children}
     </div>
   );
 }
